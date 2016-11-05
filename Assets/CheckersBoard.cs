13f6de7900bc4beb53b185bc09afbb7ed9a078b3
Dan01001 <dmmajewski@gmail.com>
2016-11-05 18:57:39 -0400

    Initial Setup

---
+++ Assets/CheckersBoard.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckersBoard : MonoBehaviour
+{
+
+    public Piece[,] pieces = new Piece[8, 8];
+    public GameObject whitePiecePrefab;
+    public GameObject blackPiecePrefab;
+    public Vector3 BoardOffset = new Vector3(-4.0f, 0, -4.0f);
+
+    void start()
+    {
+        GenerateBoard();
+    }
+
+    private void GenerateBoard()
+    {
+        //Generate White Team
+        for(int y = 0; y < 3; y++)
+        {
+            for(int x= 0; x<8; x+= 2)
+            {
+                //Generate out piece
+                GeneratePiece(x, y);
+            }
+        }
+    }
+
+    private void GeneratePiece(int x, int y)
+    {
+        GameObject go = Instantiate(whitePiecePrefab) as GameObject;
+        go.transform.SetParent(transform);
+        Piece p = go.GetComponent<Piece>();
+        pieces[x, y] = p;
+        MovePiece(p, x, y);
+    }
+
+    void MovePiece(Piece p, int x, int y)
+    {
+        p.transform.position = (Vector3.right * x) + (Vector3.forward * y};
+}
---
+++ Assets/Piece.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece : MonoBehaviour 
+{
+
+        
+    void Start () 
+    {
+	
+    }
+	
+        
+    void Update () 
+    {
+	
+    }
+}
